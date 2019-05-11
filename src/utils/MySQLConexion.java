0aefee33f9e951984dbdd5f1842b016938d0ec35
Enrique Hermida <enriqueh.cehd@outlook.com>
2019-05-11 18:30:48 -0500

    Create MySQLConexion.java

---
+++ src/utils/MySQLConexion.java
@@ -0,0 +1,29 @@
+package utils;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class MySQLConexion {
+public static Connection getConexion() {
+		
+		Connection con = null;
+		
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			String url = "jdbc:mysql://localhost/bd_conector";
+			String usuario = "root";
+			String contraseña ="";
+			
+			con = DriverManager.getConnection(url,usuario,contraseña);		
+			
+		} catch (ClassNotFoundException e) {
+			// TODO: handle exception
+			System.out.println("Error: carga de driver fallida");
+		}catch(SQLException e) {
+			System.out.println("Error: no hay conexion con la BD");
+			e.printStackTrace();
+		}
+		return con;
+	}
+}
